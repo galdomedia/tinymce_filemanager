d0a10465e1869b08f2beaed4229aa899b20e9d48
Leszek Smentek <leszek.smentek@galdomedia.pl>
2010-06-24 12:14:37 +0200

    translation add (en, pl by now), save-erb problems fixed, tinyMCE translation files add

--- app/controllers/tinymce/filemanager_controller.rb
+++ app/controllers/tinymce/filemanager_controller.rb
@@ -1,6 +1,11 @@
 class Tinymce::FilemanagerController < ActionController::Base
   def tinymce_managed_js
+    @manager_lang = "en"
+    supported_languages = ["cs", "de", "en", "es", "fr", "it", "ja", "pl", "pt", "ru", "sk", "uk", "zh"]
     @manager_controller = params[:manager]
+    if supported_languages.include?(I18n.default_locale.to_s)
+      @manager_lang = I18n.default_locale.to_s
+    end
     response.headers["Content-Type"] = "text/javascript; charset=utf-8"
     render :template =>"tinymce_filemanager/tinymce_managed_js", :layout => false
   end
